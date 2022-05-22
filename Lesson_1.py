acc0ae19401c989982a8ee46d6453c4f40529387
Satyam Pathak <73696374+Satyam-193@users.noreply.github.com>
2022-05-22 14:55:24 +0530

    Basics

---
+++ Lesson_1.py
@@ -0,0 +1,25 @@
+#python basics
+print() #command to print int,float and strings.
+type(a) #command to find data type of a.
+variable_name= input("Enter your value") #ask for input from the user.
+
+
+#data types
+
+#int
+a=5
+b=4
+c=a+b
+print(c) #print 9
+type(c) # display int
+
+#float
+a=3.04
+print(a)
+
+#string
+print("hello world") #display string
+a= "hello"
+print(a*3) #display string 3 times i.e hellohellohello 
+
+
