772c684aaa63231a6301ac9e1915dcfa389d98ce
Satyam Pathak <73696374+Satyam-193@users.noreply.github.com>
2022-05-22 15:15:04 +0530

    basic operator

---
+++ lesson_2.py
@@ -0,0 +1,32 @@
+#Types of of operators:
+#assignment operators:
+
+# operator '+'
+print(3+5) # display 8
+
+# operator '-'
+a=5
+b=2
+print(a-b) #display 3
+
+# operator '*' . '//', '/'
+print(3 * 2) #print 6
+print(4 / 2) #print 2
+print(5 // 2) #print 1(remainder)
+
+# operator '**'
+a=2
+print(a ** 3) # print 8
+
+#logical operator 
+
+# operator ' > '
+print(a>b) # display either true or false for ' > ', '<', '!=' operators.
+
+# operator ' < ' ,'!='
+print(a<b)
+print(a!=b)
+
+
+
+
