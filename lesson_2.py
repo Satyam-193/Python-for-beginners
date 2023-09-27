242d60e1a361cd299c293133e9ae92ce56abf559
Satyam Pathak <73696374+Satyam-193@users.noreply.github.com>
2023-09-28 00:14:08 +0530

    Update lesson_2.py

--- lesson_2.py
+++ lesson_2.py
@@ -1,4 +1,5 @@
-#Types of of operators:
+##Types of of operators:
+
 #assignment operators:
 
 # operator '+'
@@ -9,9 +10,13 @@ a=5
 b=2
 print(a-b) #display 3
 
-# operator '*' . '//', '/'
+# operator '*'
 print(3 * 2) #print 6
+
+# operator '//'
 print(4 / 2) #print 2
+
+# operator '/'
 print(5 // 2) #print 1(remainder)
 
 # operator '**'
@@ -19,14 +24,14 @@ a=2
 print(a ** 3) # print 8
 
 #logical operator 
+# display either true or false for ' > ', '<', '!=' operators.
 
 # operator ' > '
-print(a>b) # display either true or false for ' > ', '<', '!=' operators.
+print(a>b) 
 
-# operator ' < ' ,'!='
+# operator ' < ' 
 print(a<b)
+
+# operator '!='
 print(a!=b)
-
-
-
 
